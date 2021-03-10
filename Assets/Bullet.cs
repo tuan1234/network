f37d6cce8b581ddef0725ae2fbfc35b5646954db
Minh-Tuan Ngo <minh-tuan.ngo@rentacar.fr>
2021-03-10 21:32:40 +0100

    add

---
+++ Assets/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        var hitCombat = hit.GetComponent<Combat>();
+         Debug.Log("OnCollisionEnter => collision " + hit.name);
+        if (hitCombat != null)
+        {
+             Debug.Log("OnCollisionEnter => hitCombat" + hitCombat.name);
+            hitCombat.TakeDamage(10);
+            Destroy(gameObject);
+        }
+    }
+}
